bc6102c84704ba238a222af0e3888e99083ff013
jerrf010 <huntingaiden513@gmail.com>
2025-10-03 14:11:50 -0400

    Update

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-let name = "Aiden";
-console.log(`Hello", name);
+let num1 = 6;
+let num2 = 7;
+
+console.log("The sum is: " + (num1 + num2));
+console.log("The product is: " + (num1 * num2));
+console.log("The difference is: " + (num1 - num2));
+console.log("The quotient is: " + (num1 / num2));
